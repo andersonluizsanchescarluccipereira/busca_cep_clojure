c3aedc55a8dd869095f8a4f21d5dc4ba1e8db4e2
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2026-04-26 17:27:50 -0300

    implement soap

--- src/busca_cep/http/router.clj
+++ src/busca_cep/http/router.clj
@@ -1,9 +1,10 @@
 (ns busca-cep.http.router
   (:require
-   [reitit.ring :as ring]
-   [muuntaja.core :as m]
-   [reitit.ring.middleware.muuntaja :as muuntaja]
-   [busca-cep.ports.cep-port :as port]))
+    [reitit.ring :as ring]
+    [muuntaja.core :as m]
+    [reitit.ring.middleware.muuntaja :as muuntaja]
+    [busca-cep.ports.cep-port :as port]
+    [clojure.java.io :as io]))
 
 (defn make-handler [adapter]
   (ring/ring-handler
@@ -11,21 +12,52 @@
     [["/status"
       {:get (fn [_]
               {:status 200
-               :body {:status "ok"}})}]   ;; <<< HEALTHCHECK AQUI
+               :body {:status "ok"}})}]
 
      ["/cep/:cep"
       {:get (fn [{:keys [path-params]}]
               {:status 200
-               :body   (port/fetch-cep adapter (:cep path-params))})}]
+               :body (port/fetch-cep adapter (:cep path-params))})}]
 
      ["/webhook/cep"
       {:post (fn [{:keys [body-params]}]
                (let [cep (:cep body-params)
                      result (port/fetch-cep adapter cep)]
                  {:status 200
-                  :body result}))}]]
+                  :body result}))}]
 
-    ;; Config global do Reitit
+     ;; 🔥 SOAP endpoint (AGORA EXISTE)
+     ["/soap"
+      {:post (fn [request]
+               (let [xml (slurp (:body request))
+                     _ (println "SOAP REQUEST:" xml) ;; debug útil
+                     cep (second (re-find #"<cep:cep>(.*?)</cep:cep>" xml))
+                     result (port/fetch-cep adapter cep)]
+                 {:status 200
+                  :headers {"Content-Type" "text/xml; charset=utf-8"}
+                  :body (str
+                         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                         "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+                         "<soap:Body>"
+                         "<GetCepResponse xmlns=\"http://example.com/cep\">"
+                         "<cepInfo>" result "</cepInfo>"
+                         "</GetCepResponse>"
+                         "</soap:Body>"
+                         "</soap:Envelope>")}))}]
+
+     ;; 📄 WSDL
+     ["/service"
+      {:get (fn [req]
+              (let [qs (:query-string req)]
+                (if (and qs (re-find #"(^|&)wsdl(=|&|$)" qs))
+                  (let [res (io/resource "wsdl/service.wsdl")]
+                    (if res
+                      {:status 200
+                       :headers {"Content-Type" "text/xml; charset=utf-8"}
+                       :body (slurp res)}
+                      {:status 404 :body "WSDL not found"}))
+                  {:status 404 :body "Not Found"})))}]]
+
     {:data {:muuntaja m/instance
             :middleware [muuntaja/format-negotiate-middleware
                          muuntaja/format-response-middleware
